9f448bb2effb3b271ef6a0edcbd6507747a3c0d1
Your Name <you@example.com>
2019-11-20 19:58:59 +0000

    finished on planning for deployment

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+define(function() {var keywords=[{w:"About",p:["p0"]},{w:"this",p:["p0"]},{w:"publication",p:["p0"]},{w:"Contacting",p:["p1"]},{w:"IBM",p:["p1","p2","p3"]},{w:"StoredIQ",p:["p1","p2","p3"]},{w:"customer",p:["p1"]},{w:"support",p:["p1"]},{w:"Applications",p:["p2"]},{w:"of",p:["p2"]},{w:"components",p:["p3","p4"]},{w:"Solution",p:["p4"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[8, 9, 4, 5];
+ph["p3"]=[4, 5, 10];
+ph["p4"]=[11, 10];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
