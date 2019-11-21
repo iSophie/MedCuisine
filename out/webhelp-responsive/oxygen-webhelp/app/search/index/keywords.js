96860410ea95d031d50e9da504dedc9356c278e6
Your Name <you@example.com>
2019-11-21 21:30:32 +0000

    created safety.dita but hazard doesnt display in pdf transformation

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,10 +1,13 @@
-define(function() {var keywords=[{w:"About",p:["p0"]},{w:"this",p:["p0"]},{w:"publication",p:["p0"]},{w:"Contacting",p:["p1"]},{w:"IBM",p:["p1","p2","p3"]},{w:"StoredIQ",p:["p1","p2","p3"]},{w:"customer",p:["p1"]},{w:"support",p:["p1"]},{w:"Applications",p:["p2"]},{w:"of",p:["p2"]},{w:"components",p:["p3","p4"]},{w:"Solution",p:["p4"]}];
+define(function() {var keywords=[{w:"About",p:["p0"]},{w:"this",p:["p0"]},{w:"publication",p:["p0"]},{w:"Contacting",p:["p1"]},{w:"IBM",p:["p1","p2","p3","p4"]},{w:"StoredIQ",p:["p1","p2","p3","p4"]},{w:"customer",p:["p1"]},{w:"support",p:["p1"]},{w:"product",p:["p2"]},{w:"library",p:["p2"]},{w:"Applications",p:["p3"]},{w:"of",p:["p3"]},{w:"components",p:["p4","p7"]},{w:"Open",p:["p5"]},{w:"Virtual",p:["p5"]},{w:"Appliance",p:["p5"]},{w:"(OVA)",p:["p5"]},{w:"configuration",p:["p5"]},{w:"requirements",p:["p5"]},{w:"Planning",p:["p6"]},{w:"for",p:["p6"]},{w:"deployment",p:["p6"]},{w:"Solution",p:["p7"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5, 6, 7];
-ph["p2"]=[8, 9, 4, 5];
-ph["p3"]=[4, 5, 10];
-ph["p4"]=[11, 10];
+ph["p2"]=[4, 5, 8, 9];
+ph["p3"]=[10, 11, 4, 5];
+ph["p4"]=[4, 5, 12];
+ph["p5"]=[13, 14, 15, 16, 17, 18];
+ph["p6"]=[19, 20, 21];
+ph["p7"]=[22, 12];
      return {
          keywords: keywords,
          ph: ph
--- out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {4:3,0:-1,2:3,1:0,3:-1};
+return {4:-1,0:-1,2:0,5:6,6:-1,7:4,3:4,1:0};
 });
